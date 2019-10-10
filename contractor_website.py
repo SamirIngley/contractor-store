0223ecd5237c50f3be9d08e688a1d51c72249921
SamirIngley <samir.ingle7@gmail.com>
2019-10-09 20:18:53 -0700

    CRUD in progress

--- contractor_website.py
+++ contractor_website.py
@@ -1,51 +1,113 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-#from bson.object_id import ObjectId
+from bson.object_id import ObjectId
 
 app = Flask(__name__)
 
-missions = [
-    {
-        "Name":"Mars",
-        "Time":"asdfasf",
-        "Terrain":"asdfadf",
-        "Volatility":"n/10",
-        "Life":"No"
-    },
-    {
-        "Name":"Earth",
-        "Time":"asdfasf",
-        "Terrain":"asdfadf",
-        "Volatility":"n/10",
-        "Life":"Human"
-    }
-]
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor_website_db')
+client = MongoClient(host=f"{host}?retryWrites=false")
+db = client.get_default_database()
 
-products =[
-    {
-        "Type":"SpaceSuit",
-        "Name":"Jumpman",
-        "Price":"out of this world",
-        "Specs":"anti-gravity kit, flexible materials"
-    },
-    {
-        "Type":"Spacesuit",
-        "Name":"Lil' Pinch",
-        "Price":"underground",
-        "Specs":"power claws, weight distribution"
-    }
-]
+space_suits = db.suits
+
+# missions = [
+#     {
+#         "Name":"Mars",
+#         "Time":"asdfasf",
+#         "Terrain":"asdfadf",
+#         "Volatility":"n/10",
+#         "Life":"No"
+#     },
+#     {
+#         "Name":"Earth",
+#         "Time":"asdfasf",
+#         "Terrain":"asdfadf",
+#         "Volatility":"n/10",
+#         "Life":"Human"
+#     }
+# ]
+#
+# products =[
+#     {
+#         "Type":"SpaceSuit",
+#         "Name":"Jumpman",
+#         "Price":"out of this world",
+#         "Specs":"anti-gravity kit, flexible materials"
+#     },
+#     {
+#         "Type":"Spacesuit",
+#         "Name":"Lil' Pinch",
+#         "Price":"underground",
+#         "Specs":"power claws, weight distribution"
+#     }
+# ]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route("/mission")
 def mission():
     return render_template('mission.html', products=products)
+
+@app.route("/products")
+def products():
+    return render_template('products.html')
+
+
+
+@app.route('/new')
+def new_suit():
+    """Return new suit creation page."""
+    return render_template('new_suit.html')
+
+@app.route('/new', methods=['POST'])
+def create_suit():
+    """Make a new suit posting according to user's specifications."""
+    suit = {
+        'name': request.form.get('name'),
+        'price': request.form.get('price'),
+        'img_url': request.form.get('img_url')
+    }
+    suit_id = space_suits.insert_one(suit).inserted_id
+    return redirect(url_for('show_suit', suit_id=suit_id))
+
+@app.route('/suit/<suit_id>')
+def show_suit(suit_id):
+    """Show a single suit."""
+    suit = space_suits.find_one({'_id': ObjectId(suit_id)})
+    return render_template('show_candy.html', suit=suit)
+
+@app.route('/edit/<candy_id>', methods=['POST'])
+def update_suit(suit_id):
+    """Edit page for a candy."""
+    new_suit = {
+        'name': request.form.get('name'),
+        'price': request.form.get('price'),
+        'img_url': request.form.get('img_url')
+    }
+    space_suits.update_one(
+        {'_id': ObjectId(suit_id)},
+        {'$set': new_suit}
+    )
+    return redirect(url_for('show_suit', suit_id=suit_id))
+
+@app.route('/delete/<suit_id>', methods=['POST'])
+def delete_suit(suit_id):
+    """Delete a candy."""
+    space_suits.delete_one({'_id': ObjectId(suit_id)})
+    return redirect(url_for('index'))
+
+@app.route('/edit/<suit_id>', methods=['GET'])
+def edit_candy(candy_id):
+    """Page to submit an edit on a candy."""
+    suit = space_suits.find_one({'_id': ObjectId(space_id)})
+    return render_template('edit_suit.html', suit=suit)
+
+
 
 
 if __name__ == '__main__':
