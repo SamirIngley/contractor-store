8b3b720536ea479b9458c32dce7a5ea9f05b9375
SamirIngley <samir.ingle7@gmail.com>
2019-10-10 17:12:17 -0700

    tests

--- tests.py
+++ tests.py
@@ -2,9 +2,9 @@ from unittest import TestCase, main as unittest_main, mock
 from bson.objectid import ObjectId
 from app import app
 
-sample_playlist_id = ObjectId('5d55cffc4a3d4031f42827a3')
+sample_suit_id = ObjectId('5d55cffc4a3d4031f42827a3')
 
-products =[
+sample_suit =[
     {
         "Name":"Jumpman",
         "Price":"2000000",
@@ -17,3 +17,59 @@ products =[
         "Specs":"power claws, weight distribution"
     }
 ]
+
+class ContractorTests(TestCase):
+    """Flask tests."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+
+    def test_index(self):
+        """Test the suits homepage."""
+        result = self.client.get('/')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Suits Homepage', result.data)
+
+    def test_new(self):
+        """Test the new playlist creation page."""
+        result = self.client.get('/new')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'New Suit', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_show_suit(self, mock_find):
+        """Test showing a single suit."""
+        mock_find.return_value = sample_playlist
+
+        result = self.client.get(f'/suit/{sample_suit_id}')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Jumpman', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_edit_suit(self, mock_find):
+        """Test showing a single playlist."""
+        mock_find.return_value = sample_suit
+
+        result = self.client.get(f'/suits/edit/{sample_suit_id}')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Jumpman', result.data)
+
+    #
+    # @mock.patch('pymongo.collection.Collection.insert_one')
+    # def test_submit_playlist(self, mock_insert):
+    #     """Test submitting a new suit."""
+    #     result = self.client.post('/suits', data=sample_form_data)
+    #
+    #     # After submitting, should redirect to that playlist's page
+    #     self.assertEqual(result.status, '302 FOUND')
+    #     mock_insert.assert_called_with(sample_suit)
+
+
+if __name__ == '__main__':
+    unittest_main()
